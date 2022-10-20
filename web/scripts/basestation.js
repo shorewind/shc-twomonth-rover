9c16ede9fa864ee7b69803c6262bf4f80aa6a103
jdww5567 <jadaniel06@gmail.com>
2022-10-19 19:32:10 -0500

    Basic chart.js stuff

--- web/scripts/basestation.js
+++ web/scripts/basestation.js
@@ -144,4 +144,20 @@ function connect_rosbridge() {
     ros.connect(address);
 }
 
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
+
 window.onload = setup;
